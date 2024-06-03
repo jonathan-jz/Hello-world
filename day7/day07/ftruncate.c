0ee78dd166c2078ba87131530e69baf7707d50a3
zhangshier <jonathonzhang16@gmail.com>
2024-06-03 21:20:57 +0800

    hh

--- day7/day07/ftruncate.c
+++ day7/day07/ftruncate.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]){
     int fd = open(argv[1], O_RDWR);
     ERROR_CHECK(fd,-1,"open");
     printf("fd = %d\n", fd);
-    int ret = ftruncate(fd,2);
+    int ret = ftruncate(fd,24);
     printf("%d\n",ret);
     ERROR_CHECK(ret,-1,"ftruncate");
     close(fd);  
