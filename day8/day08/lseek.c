050b239d6de513d909d9a1e4c4faa90ae9d8ccc2
zhangshier <jonathonzhang16@gmail.com>
2024-06-04 15:25:26 +0800

    del

--- day8/day08/lseek.c
+++ day8/day08/lseek.c
@@ -4,7 +4,7 @@ int main(int argc,char*argv[]){
     ARGS_CHECK(argc,2);
     int fd = open(argv[1],O_CREAT|O_RDWR,0666);
     ERROR_CHECK(fd,-1,"open");
-    lseek(fd,100,SEEK_SET);
+    lseek(fd,10,SEEK_SET);
     write(fd,"hello",5);
     close(fd);
 
